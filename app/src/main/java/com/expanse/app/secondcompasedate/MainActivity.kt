70b2182c7deaafe20aef034411161c60a4be2f69
Osagie Omon <cjaeomon@gmail.com>
2021-09-09 13:43:04 +0100

    Extend custom Text

--- app/src/main/java/com/expanse/app/secondcompasedate/MainActivity.kt
+++ app/src/main/java/com/expanse/app/secondcompasedate/MainActivity.kt
@@ -18,8 +18,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.expanse.app.secondcompasedate.ui.theme.SecondCompaseDateTheme
 import com.google.accompanist.coil.rememberCoilPainter
@@ -43,64 +47,50 @@ fun LayoutsCodelab() {
             )
         }
     ) {
-        ScrollingList()
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
     }
 }
 
-@Composable
-fun ScrollingList() {
-    val listSize = 100
-    // We save the scrolling position with this state
-    val scrollState = rememberLazyListState()
-    // We save the coroutine scope where our animated scroll will be executed
-    val coroutineScope = rememberCoroutineScope()
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
 
-    Column {
-        Row {
-            Button(onClick = {
-                coroutineScope.launch {
-                    // 0 is the first item index
-                    scrollState.animateScrollToItem(0)
-                }
-            }) {
-                Text("Scroll to the top")
-            }
+        // Check the composable has a first baseline
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
 
-            Button(onClick = {
-                coroutineScope.launch {
-                    // listSize - 1 is the last index of the list
-                    scrollState.animateScrollToItem(listSize - 1)
-                }
-            }) {
-                Text("Scroll to the end")
-            }
-        }
-
-        LazyColumn(state = scrollState) {
-            items(listSize) {
-                ImageListItem(it)
-            }
+        // Height of the composable with padding - first baseline
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            // Where the composable gets placed
+            placeable.placeRelative(0, placeableY)
         }
     }
-}
+)
 
+@Preview
 @Composable
-fun ImageListItem(index: Int) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Image(
-            painter = rememberCoilPainter("https://developer.android.com/images/brand/Android_Robot.png"),
-            contentDescription = "Android Logo",
-            modifier = Modifier.size(50.dp)
-        )
-        Spacer(Modifier.width(10.dp))
-        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
+fun TextWithPaddingToBaselinePreview() {
     SecondCompaseDateTheme {
-        LayoutsCodelab()
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
     }
 }
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    SecondCompaseDateTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
+    }
+}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    SecondCompaseDateTheme {
+//        LayoutsCodelab()
+//    }
+//}

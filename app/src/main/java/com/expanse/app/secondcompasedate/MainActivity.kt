8324fa743da76758c8c6abfe62aa64aba89551c9
Osagie Omon <cjaeomon@gmail.com>
2021-09-10 13:29:26 +0100

    Custom column layout

--- app/src/main/java/com/expanse/app/secondcompasedate/MainActivity.kt
+++ app/src/main/java/com/expanse/app/secondcompasedate/MainActivity.kt
@@ -3,31 +3,16 @@ package com.expanse.app.secondcompasedate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.AlignmentLine
-import androidx.compose.ui.layout.FirstBaseline
-import androidx.compose.ui.layout.layout
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.expanse.app.secondcompasedate.ui.theme.SecondCompaseDateTheme
-import com.google.accompanist.coil.rememberCoilPainter
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,51 +31,56 @@ fun LayoutsCodelab() {
                 title = { Text(text = "LayoutsCodelab") }
             )
         }
-    ) {
-        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
     }
 }
 
-fun Modifier.firstBaselineToTop(
-    firstBaselineToTop: Dp
-) = this.then(
-    layout { measurable, constraints ->
-        val placeable = measurable.measure(constraints)
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
+    }
+}
 
-        // Check the composable has a first baseline
-        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
-        val firstBaseline = placeable[FirstBaseline]
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
 
-        // Height of the composable with padding - first baseline
-        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
-        val height = placeable.height + placeableY
-        layout(placeable.width, height) {
-            // Where the composable gets placed
-            placeable.placeRelative(0, placeableY)
+        // Don't constrain child views further, measure them with given constraints
+        // List of measured children
+        val placeables = measurables.map { measurable ->
+            // Measure each child
+            measurable.measure(constraints)
+        }
+
+        var yPosition = 0
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            // Place children in the parent layout
+            placeables.forEach { placeable ->
+                // Position item on the screen
+                placeable.placeRelative(x = 0, y = yPosition)
+
+                // Record the y co-ord placed up to
+                yPosition += placeable.height
+            }
         }
     }
-)
-
-@Preview
-@Composable
-fun TextWithPaddingToBaselinePreview() {
-    SecondCompaseDateTheme {
-        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
-    }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-fun TextWithNormalPaddingPreview() {
+fun DefaultPreview() {
     SecondCompaseDateTheme {
-        Text("Hi there!", Modifier.padding(top = 32.dp))
+        LayoutsCodelab()
     }
 }
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    SecondCompaseDateTheme {
-//        LayoutsCodelab()
-//    }
-//}
